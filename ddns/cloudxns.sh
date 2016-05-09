c08314ef5a254f9cba4bf832a177bf70ed0965ff
paranoiagu <gu.yanfeng@gmail.com>
2016-05-09 18:40:56 +0800

    HOST 可以支持多个
    支持一个域名下多个 HOST 的更新。

--- ddns/cloudxns.sh
+++ ddns/cloudxns.sh
@@ -28,23 +28,31 @@ DOMAIN_ID=$(curl -k -s $URL_D -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE
 echo "DOMAIN ID: "$DOMAIN_ID
 
 URL_R="https://www.cloudxns.net/api2/record/$DOMAIN_ID?host_id=0&row_num=500"
-HMAC_R=$(echo -n "$API_KEY$URL_R$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
-RECORD_ID=$(curl -k -s $URL_R -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_R"|grep -o "record_id\":\"[0-9]*\",\"host_id\":\"[0-9]*\",\"host\":\"$HOST\""|grep -o "record_id\":\"[0-9]*"|grep -o "[0-9]*")
 
-echo "RECORD ID: "$RECORD_ID
 
-URL_U="https://www.cloudxns.net/api2/record/$RECORD_ID"
+NUM_HOSTS=$((`echo $HOST | tr -cd ',' | wc -c`+1))
+for i in `seq $NUM_HOSTS`; do
+	ONEHOST=`echo $HOST | cut -d ',' -f$i`
+	
+	HMAC_R=$(echo -n "$API_KEY$URL_R$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
 
-PARAM_BODY="{\"domain_id\":\"$DOMAIN_ID\",\"host\":\"$HOST\",\"value\":\"$IP\"}"
-HMAC_U=$(echo -n "$API_KEY$URL_U$PARAM_BODY$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
+	RECORD_ID=$(curl -k -s $URL_R -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_R"|grep -o "record_id\":\"[0-9]*\",\"host_id\":\"[0-9]*\",\"host\":\"$ONEHOST\""|grep -o "record_id\":\"[0-9]*"|grep -o "[0-9]*")
 
-RESULT=$(curl -k -s $URL_U -X PUT -d $PARAM_BODY -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_U" -H 'Content-Type: application/json')
+	echo "RECORD ID: "$RECORD_ID
 
-echo $RESULT
+	URL_U="https://www.cloudxns.net/api2/record/$RECORD_ID"
 
-if [ $(echo -n "$RESULT"|grep -o "message\":\"success\""|wc -l) = 1 ];then
-    echo "$(date) -- Update success"
-    echo "LAST_IP=\"$IP\"" > $LAST_IP_FILE
-else
-    echo "$(date) -- Update failed"
-fi
+	PARAM_BODY="{\"domain_id\":\"$DOMAIN_ID\",\"host\":\"$ONEHOST\",\"value\":\"$IP\"}"
+	HMAC_U=$(echo -n "$API_KEY$URL_U$PARAM_BODY$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
+
+	RESULT=$(curl -k -s $URL_U -X PUT -d $PARAM_BODY -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_U" -H 'Content-Type: application/json')
+
+	echo $RESULT
+
+	if [ $(echo -n "$RESULT"|grep -o "message\":\"success\""|wc -l) = 1 ];then
+		echo "$(date) -- Update success"
+		echo "LAST_IP=\"$IP\"" > $LAST_IP_FILE
+	else
+		echo "$(date) -- Update failed"
+	fi
+done
